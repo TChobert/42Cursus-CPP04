81e314563492c730db5fb476c9927bd40418aaa6
Thomas Chobert <tchobert@f1r4s19.clusters.42paris.fr>
2025-05-26 17:27:16 +0200

    ex00 - Animal canonic

---
+++ ex00/Animal.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchobert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/26 17:05:28 by tchobert          #+#    #+#             */
+/*   Updated: 2025/05/26 17:05:59 by tchobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <cstdlib>
+# include <iostream>
+# include <string>
+
+class	Animal {
+
+	protected:
+
+	std::string	_type;
+
+	public:
+
+	Animal(void);
+	Animal(const Animal& other);
+	Animal&	operator=(const Animal& other);
+	~Animal(void);
+
+	virtual void	makeSound(void) const;
+	std::string		getType(void) const;
+	void			setType(const std::string& type);
+};
+
+#endif
